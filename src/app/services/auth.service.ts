bc9ff328ab545a80b583b5b97b4bb70c74325f0b
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-11-26 19:40:01 +0100

    daily

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  // Function to log in user
+
   login(email: string, password: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/login`, { email, password }).pipe(
       tap((response: any) => {
@@ -27,8 +27,8 @@ export class AuthService {
         }
       }),
       catchError(error => {
-        console.error('Login error:', error); // Log any error during the login
-        return of(null); // Return null to indicate failure
+
+        return of(null); 
       })
     );
   }
